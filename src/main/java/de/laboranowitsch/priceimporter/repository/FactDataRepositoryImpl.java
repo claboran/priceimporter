0e2874d67713d0416424b96900da5acb3d3674f2
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-04-30 09:17:18 +0200

    FactData Repo  extended

--- src/main/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryImpl.java
+++ src/main/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryImpl.java
@@ -32,13 +32,15 @@ public class FactDataRepositoryImpl implements FactDataRepository {
     private static final String D_PERIOD_ID_PARAM = "d_period_type_id";
     private static final String D_DATE_TIME_ID_PARAM = "d_date_time_id";
 
-    private static final String FACT_DATA_QUERY = "select * from f_energy_price_demand ";
+    private static final String FACT_DATA_QUERY = "select * from f_energy_price_demand where ";
     private static final String INSERT_STMT = "insert into f_energy_price_demand (";
+    private static final String UPDATE_STMT = "update f_energy_price_demand set ";
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
     private final SequenceGenerator sequenceGenerator;
     private final String factDataQuery;
     private final String insertStmt;
+    private final String updateStmt;
     private final String sequenceName;
 
     @Autowired
@@ -50,9 +52,11 @@ public class FactDataRepositoryImpl implements FactDataRepository {
         this.sequenceGenerator = sequenceGenerator;
         this.factDataQuery = createFactDataQuery().replace("f_energy_price_demand", factDataTableName);
         this.insertStmt = createFactDataInsertStmt().replace("f_energy_price_demand", factDataTableName);
+        this.updateStmt = createFactDataUpdateStmt().replace("f_energy_price_demand", factDataTableName);
         this.sequenceName = sequenceName;
         LOG.info("FactData query: {}", this.factDataQuery);
         LOG.info("FactData insert statement: {}", this.insertStmt);
+        LOG.info("FactData update statement: {}", this.updateStmt);
     }
 
     @Override
@@ -88,12 +92,20 @@ public class FactDataRepositoryImpl implements FactDataRepository {
         return nextVal;
     }
 
+    private void update(FactData factData) {
+        MapSqlParameterSource mapSqlParameterSource = createUpdateStmtParameterMap(factData);
+        jdbcTemplate.update(updateStmt, mapSqlParameterSource);
+    }
+
     @Override
     public Long save(FactData factData) {
         FactData result = findByFactData(factData);
         if(result == null) {
             return saveNew(factData);
         }
+        result.setTotalDemand(factData.getTotalDemand());
+        result.setRpr(factData.getRpr());
+        update(result);
         return result.getId();
     }
 
@@ -107,6 +119,13 @@ public class FactDataRepositoryImpl implements FactDataRepository {
     private MapSqlParameterSource createInsertStmtParameterMap(FactData factData) {
         MapSqlParameterSource sqlParameterSource = createQueryParameterMap(factData);
         sqlParameterSource.addValue(ID_PARAM, factData.getId());
+        sqlParameterSource.addValue(TOTAL_DEMAND_PARAM, factData.getTotalDemand());
+        sqlParameterSource.addValue(RPR_PARAM, factData.getRpr());
+        return sqlParameterSource;
+    }
+
+    private MapSqlParameterSource createUpdateStmtParameterMap(FactData factData) {
+        MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource(ID_PARAM, factData.getId());
         sqlParameterSource.addValue(TOTAL_DEMAND_PARAM, factData.getTotalDemand());
         sqlParameterSource.addValue(RPR_PARAM, factData.getRpr());
         return sqlParameterSource;
@@ -139,6 +158,16 @@ public class FactDataRepositoryImpl implements FactDataRepository {
                 .append(ActualParameterHelper.makeActualParameterOf(D_PERIOD_ID_PARAM) + ", ")
                 .append(ActualParameterHelper.makeActualParameterOf(D_DATE_TIME_ID_PARAM))
                 .append(")")
+                .toString();
+    }
+
+    private String createFactDataUpdateStmt() {
+        StringBuilder stmt = new StringBuilder(UPDATE_STMT);
+        return stmt.append(TOTAL_DEMAND_PARAM  + " = " + ActualParameterHelper.makeActualParameterOf(TOTAL_DEMAND_PARAM) + ", ")
+                .append(RPR_PARAM  + " = " + ActualParameterHelper.makeActualParameterOf(RPR_PARAM))
+                .append(" where ")
+                .append(ID_PARAM + " = ")
+                .append(ActualParameterHelper.makeActualParameterOf(ID_PARAM))
                 .toString();
     }
 
--- src/test/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryTests.java
+++ src/test/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryTests.java
@@ -1,8 +1,13 @@
 package de.laboranowitsch.priceimporter.repository;
 
 import de.laboranowitsch.priceimporter.PriceImporterApplication;
+import de.laboranowitsch.priceimporter.domain.DateTime;
+import de.laboranowitsch.priceimporter.domain.FactData;
+import de.laboranowitsch.priceimporter.domain.Period;
+import de.laboranowitsch.priceimporter.domain.Region;
 import de.laboranowitsch.priceimporter.repository.sequence.SequenceGenerator;
 import de.laboranowitsch.priceimporter.util.dbloader.DbLoader;
+import org.aspectj.apache.bcel.util.ClassLoaderRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,13 +16,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Tests for {@link FactDataRepository} and {@link SequenceGenerator}
@@ -56,4 +63,36 @@ public class FactDataRepositoryTests {
         assertThat("wiring of dependencies is done properly", regionRepository, is(not(nullValue())));
     }
 
+    @Transactional
+    @Test
+    public void testSequenceGenerator() {
+        assertThat("sequence has 1", sequenceGenerator.getNextSequence("int_test_fact_seq"), is(equalTo(1L)));
+        assertThat("sequence has 2", sequenceGenerator.getNextSequence("int_test_fact_seq"), is(equalTo(2L)));
+    }
+    @Transactional
+    @Test
+    public void testInsertNew() {
+        Map<String, Long> dimensionMap = createDimensions();
+        assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .rpr(23.456)
+                .totalDemand(567.89)
+                .build()), is(equalTo(1L)));
+    }
+    private Map<String, Long> createDimensions() {
+        Map<String, Long> dimensionMap = new HashMap<>();
+        Long id = dateTimeRepository.save(DateTime.builder().dayOfMonth(1)
+                .monthOfYear(1)
+                .year(2016)
+                .hourOfDay(00)
+                .minuteOfHour(00)
+                .build());
+        dimensionMap.put("dateTimeId", id);
+        id = periodRepository.save(Period.builder().period("TRADE").build());
+        dimensionMap.put("periodId", id);
+        id = regionRepository.save(Region.builder().region("NSW").build());
+        dimensionMap.put("regionId", id);
+        return dimensionMap;
+    }
 }

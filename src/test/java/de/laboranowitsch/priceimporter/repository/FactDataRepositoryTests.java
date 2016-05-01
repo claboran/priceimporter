713a8cfcb1dd21811d70e8e3a550e470552d5300
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-05-01 12:26:11 +0200

    FactData Repo  finished

--- src/test/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryTests.java
+++ src/test/java/de/laboranowitsch/priceimporter/repository/FactDataRepositoryTests.java
@@ -7,7 +7,6 @@ import de.laboranowitsch.priceimporter.domain.Period;
 import de.laboranowitsch.priceimporter.domain.Region;
 import de.laboranowitsch.priceimporter.repository.sequence.SequenceGenerator;
 import de.laboranowitsch.priceimporter.util.dbloader.DbLoader;
-import org.aspectj.apache.bcel.util.ClassLoaderRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,7 +23,6 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Tests for {@link FactDataRepository} and {@link SequenceGenerator}
@@ -37,6 +35,8 @@ import static org.hamcrest.Matchers.equalTo;
 public class FactDataRepositoryTests {
 
     private static final Logger LOG = LoggerFactory.getLogger(FactDataRepositoryTests.class);
+    public static final String TRADE = "TRADE";
+    public static final String PD = "PD";
     @Autowired
     private DbLoader dbLoader;
     @Autowired
@@ -69,18 +69,90 @@ public class FactDataRepositoryTests {
         assertThat("sequence has 1", sequenceGenerator.getNextSequence("int_test_fact_seq"), is(equalTo(1L)));
         assertThat("sequence has 2", sequenceGenerator.getNextSequence("int_test_fact_seq"), is(equalTo(2L)));
     }
+
     @Transactional
     @Test
     public void testInsertNew() {
-        Map<String, Long> dimensionMap = createDimensions();
+        Map<String, Long> dimensionMap = createDimensions(TRADE);
         assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
                 .periodId(dimensionMap.get("periodId"))
                 .regionId(dimensionMap.get("regionId"))
-                .rpr(23.456)
+                .rpr(23.45)
                 .totalDemand(567.89)
                 .build()), is(equalTo(1L)));
+        FactData factData = factDataRepository.findByFactData(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .build());
+        assertThat("contains the right totalDemand", factData.getTotalDemand(), is(closeTo(567.89, 0.0001)));
+        assertThat("contains the right rpr", factData.getRpr(), is(closeTo(23.45, 0.0001)));
     }
-    private Map<String, Long> createDimensions() {
+
+    @Transactional
+    @Test
+    public void testInsert2New() {
+        Map<String, Long> dimensionMap = createDimensions(TRADE);
+        assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .rpr(23.45)
+                .totalDemand(567.89)
+                .build()), is(equalTo(1L)));
+        FactData factData = factDataRepository.findByFactData(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .build());
+        assertThat("contains the right totalDemand", factData.getTotalDemand(), is(closeTo(567.89, 0.0001)));
+        assertThat("contains the right rpr", factData.getRpr(), is(closeTo(23.45, 0.0001)));
+
+        dimensionMap = createDimensions(PD);
+        assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .rpr(20.12)
+                .totalDemand(550.89)
+                .build()), is(equalTo(2L)));
+        factData = factDataRepository.findByFactData(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .build());
+        assertThat("contains the right totalDemand", factData.getTotalDemand(), is(closeTo(550.89, 0.0001)));
+        assertThat("contains the right rpr", factData.getRpr(), is(closeTo(20.12, 0.0001)));
+    }
+
+    @Transactional
+    @Test
+    public void testUpdate() {
+        Map<String, Long> dimensionMap = createDimensions(TRADE);
+        assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .rpr(23.45)
+                .totalDemand(567.89)
+                .build()), is(equalTo(1L)));
+        FactData factData = factDataRepository.findByFactData(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .build());
+        assertThat("contains the right totalDemand", factData.getTotalDemand(), is(closeTo(567.89, 0.0001)));
+        assertThat("contains the right rpr", factData.getRpr(), is(closeTo(23.45, 0.0001)));
+
+        assertThat("one entry has been inserted", factDataRepository.save(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .rpr(19.45)
+                .totalDemand(500.00)
+                .build()), is(equalTo(1L)));
+
+        factData = factDataRepository.findByFactData(FactData.builder().dateTimeId(dimensionMap.get("dateTimeId"))
+                .periodId(dimensionMap.get("periodId"))
+                .regionId(dimensionMap.get("regionId"))
+                .build());
+        assertThat("contains the right totalDemand", factData.getTotalDemand(), is(closeTo(500.00, 0.0001)));
+        assertThat("contains the right rpr", factData.getRpr(), is(closeTo(19.45, 0.0001)));
+    }
+
+    private Map<String, Long> createDimensions(String periodType) {
         Map<String, Long> dimensionMap = new HashMap<>();
         Long id = dateTimeRepository.save(DateTime.builder().dayOfMonth(1)
                 .monthOfYear(1)
@@ -89,7 +161,7 @@ public class FactDataRepositoryTests {
                 .minuteOfHour(00)
                 .build());
         dimensionMap.put("dateTimeId", id);
-        id = periodRepository.save(Period.builder().period("TRADE").build());
+        id = periodRepository.save(Period.builder().period(periodType).build());
         dimensionMap.put("periodId", id);
         id = regionRepository.save(Region.builder().region("NSW").build());
         dimensionMap.put("regionId", id);
